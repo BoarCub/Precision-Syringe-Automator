f3ae04c331505e6d1415449f0f110aabd0652f5c
BoarCub <dsanyal@live.com>
2019-07-24 14:41:45 -0700

    organized screens and added file chooser with bug! - Aniket

--- WindowManager.py
+++ WindowManager.py
@@ -1,6 +1,7 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.uix.dropdown import DropDown
 
 class MainWindow(Screen):
     pass
@@ -11,8 +12,12 @@ class NewRoutineWindow(Screen):
 class NewTaskWindow(Screen):
     pass
 
+class FileChooserWindow(Screen):
+    pass
+
 class WindowManager(ScreenManager):
     pass
+
 
 kv = Builder.load_file("my.kv")
 
