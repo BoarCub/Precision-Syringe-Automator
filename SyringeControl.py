3d9b6741e02205999e34cb7594863df1ae1be6a4
s-dasa <theaniketdas@gmail.com>
2019-07-25 13:10:02 -0700

    extra files

--- SyringeControl.py
+++ SyringeControl.py
@@ -1,6 +1,8 @@
 class SyringeController(object):
     def __init__(self):
-    self.ser = Serial('dev/ttyS0', 
+        self.ser = Serial('dev/ttyS0', 38400)
     
-    def initialize
-                      
+    def initialize(direction):
+        self.ser.write(b"IZ", direction.encode(), b"\r")
+        
+        
