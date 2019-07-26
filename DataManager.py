c7f74c4aa3c5ae7f8e5e096a62c4eee9ca64ef63
Aniket Das <31552349+s-dasa@users.noreply.github.com>
2019-07-26 15:59:09 -0700

    decoder

--- DataManager.py
+++ DataManager.py
@@ -4,9 +4,19 @@ class FileImporter:
     
     def __init__ (self):
         self.filePath = None
+        self.all_commands = {}
+        self.getCommands()
+        self.rev_commands = self.reverseDictionary(self.all_commands)
+        self.actions = []
         
     def setPath(self, path):
         self.filePath = path
+
+    def reverseDictionary(self, inputlist):
+        newDict = {}
+        for colorString in inputlist:
+            newDict.update({inputlist[colorString]: colorString})
+        return newDict
         
     def importFile(self):
         
@@ -24,14 +34,31 @@ class FileImporter:
     def getCommands(self):
         try:
             with open(os.path.dirname(os.path.realpath(__file__)) + "/CommandsDatabase") as file:
-                all_commands =  json.load(file)
-            return all_commands
+                self.all_commands =  json.load(file)
+            return self.all_commands
 
         except FileNotFoundError:
             print("Command Database file not found.")
             
     def parseImportedString(self, stringToParse):
-        
+        for char in range(0, len(stringToParse)):
+            if stringToParse[char].isalpha():
+                index = 1
+                current_int = ""
+                try:
+                    while(stringToParse[char+index].isdigit()):
+                        current_int += stringToParse[char + index]
+                        self.actions.append(self.rev_commands[stringToParse[char]] + ": " + str(int(current_int))+", ")
+                        index +=1
+                except ValueError:
+                    self.actions.append(self.rev_commands[stringToParse[char]]+", ")
+                    pass
+                except IndexError:
+                    break
+        print("actions are ", self.actions)
+        return self.actions
+
             
 FileImporter = FileImporter()
+FileImporter.parseImportedString("Z2O86G6O1")
 
