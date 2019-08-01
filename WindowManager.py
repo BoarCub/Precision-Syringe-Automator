6e3e927293bc809631cf34ca7b4af7f9881a18d7
Deepayan Sanyal <dsanyal@live.com>
2019-08-01 10:42:58 -0700

    Set up project to work with PyCharm

--- WindowManager.py
+++ WindowManager.py
@@ -1,21 +1,20 @@
+import os
+
+from DataManager import FileImporter
+from SerialManager import *
+from TaskCreator import *
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.screenmanager import Screen, ScreenManager
-from kivy.uix.dropdown import DropDown
-from kivy.uix.widget import Widget
-from kivy.uix.label import Label
-from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.spinner import Spinner
+from kivy.uix.button import Button
+from kivy.uix.label import Label
 from kivy.uix.popup import Popup
+from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.uix.spinner import Spinner
 from kivy.uix.textinput import TextInput
-import os
-from DataManager import FileImporter
-from TaskCreator import *
 
-from SerialManager import *
 
- #each class is a window that is being called via the kv file
+#each class is a window that is being called via the kv file
 
 class MainWindow(Screen):
     pass
