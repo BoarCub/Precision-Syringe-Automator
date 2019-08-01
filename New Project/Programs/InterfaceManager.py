c729095f7a08fea406af62af34f45565634684de
Deepayan Sanyal <dsanyal@live.com>
2019-08-01 14:41:46 -0700

    Fixed build errors when running the app

--- New Project/Programs/InterfaceManager.py
+++ New Project/Programs/InterfaceManager.py
@@ -9,28 +9,29 @@ from kivy.uix.popup import Popup
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.uix.spinner import Spinner
 from kivy.uix.textinput import TextInput
-        
-        
-# Path of .kv to import the user interface from 
-KIVY_FILE = Builder.load_file("Interface.kv")
-        
 
-class MainApp(App):
-    
-    def build(self):
-        return KIVY_FILE
-    
-
-# Main function which runs the app
-if __name__ == "__main__":
-    MainApp().run()
-    
 
 # InterfaceManager acts as a container for all of the Screens in the app
 class InterfaceManager(ScreenManager):
     pass
 
 
-# Container for StartMenu Screen
+# The container for the StartScreen Screen
 class StartScreen(Screen):
     pass
+
+
+# Loads the .kv file needed
+kv = Builder.load_file("Interface.kv")
+
+
+# Builds the app
+class MainApp(App):
+
+    def build(self):
+        return kv
+
+
+# Main Function which runs the app
+if __name__ == "__main__":
+    MainApp().run()
