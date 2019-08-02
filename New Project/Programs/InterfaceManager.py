f4523ad44884b1a959656ddebef5bf69479bab43
Deepayan Sanyal <dsanyal@live.com>
2019-08-02 11:36:57 -0700

    nothing

--- New Project/Programs/InterfaceManager.py
+++ New Project/Programs/InterfaceManager.py
@@ -44,7 +44,7 @@ class RoutineCreatorScreen(Screen):
     def displayAction(index, list):
         pass
     
-    def generateValveSpinner(self, isInput:)
+    def generateValveSpinner(self, isInput):
         spinner = Spinner(
             text = 'Empty',
             values = ('1', '2', '3', '4', '5', '6', '7', '8')
