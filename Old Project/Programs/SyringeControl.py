290e97f10a0d8c3e36ae9a2dd9c7c7850538a3fc
Deepayan Sanyal <dsanyal@live.com>
2019-08-23 11:56:48 -0800

    Remove Legacy Folder
    Remove Old Project and Experimental folders

--- Old Project/Programs/SyringeControl.py
+++
@@ -1,6 +0,0 @@
-class SyringeController(object):
-    def __init__(self):
-        self.ser = Serial('dev/ttyS0', 38400)
-    
-    def initialize(direction):
-        self.ser.write(b"IZ", direction.encode(), b"\r")
--- Old Project/Programs/TaskCreator.py
+++
@@ -1,9 +0,0 @@
-class TaskCreator(object):
-    def __init__(self):
-        self.mode = "Output"
-        self.action = ""
-        self.num = 1
-        self.actions = []
-        self.buttons = []
-
-taskcreator_object = TaskCreator()
